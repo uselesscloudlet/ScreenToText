d6bfb4c0f14781f28d8b260cf533545d7590e781
uselesscloudlet <54158357+uselesscloudlet@users.noreply.github.com>
2020-05-21 18:08:47 +0300

    Добавьте файлы проекта.

---
+++ ScreenToText/ConvertImage.cs
@@ -0,0 +1,61 @@
+﻿using IronOcr;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using Tesseract;
+using System.Text.Json;
+
+namespace ScreenToText
+{
+    class ConvertImage
+    {
+        public static void ConvertImageToTextIron()
+        {
+            var Ocr = new AdvancedOcr()
+            {
+                CleanBackgroundNoise = false,
+                EnhanceContrast = true,
+                EnhanceResolution = false,
+                Strategy = IronOcr.AdvancedOcr.OcrStrategy.Advanced,
+                ColorSpace = AdvancedOcr.OcrColorSpace.Color,
+                DetectWhiteTextOnDarkBackgrounds = false,
+                InputImageType = AdvancedOcr.InputTypes.Document,
+                RotateAndStraighten = false,
+                ReadBarCodes = false,
+                ColorDepth = 4
+            };
+                //Person restoredPerson = await JsonSerializer.DeserializeAsync<Person>(fs);
+                //Console.WriteLine($"Name: {restoredPerson.Name}  Age: {restoredPerson.Age}");
+            // Ocr.Language = IronOcr.Languages.English.OcrLanguagePack;
+            Ocr.Language = new IronOcr.Languages.MultiLanguage(IronOcr.Languages.English.OcrLanguagePack, IronOcr.Languages.Russian.OcrLanguagePack);
+            /*new IronOcr.Languages.MultiLanguage(IronOcr.Languages.English.OcrLanguagePack, IronOcr.Languages.Russian.OcrLanguagePack);*/
+            Image img = Clipboard.GetImage();
+            var results = Ocr.Read(img);
+            Console.WriteLine(results.ToString());
+            Clipboard.SetText(results.ToString());
+        }
+
+        public static void ConvertImageToTextVS()
+        {
+            Image img = Clipboard.GetImage();
+            Pix resultImg = PixConverter.ToPix(Get24bppRgb(img));
+            // Image img = Clipboard.GetImage();
+            var Ocr = new TesseractEngine("./datasets", "eng", EngineMode.Default);
+            var results = Ocr.Process(resultImg);
+            Console.WriteLine(results.GetText());
+            Clipboard.SetText(results.GetText());
+        }
+
+        private static Bitmap Get24bppRgb(Image image)
+        {
+            var bitmap = new Bitmap(image);
+            var bitmap24 = new Bitmap(bitmap.Width, bitmap.Height, PixelFormat.Format24bppRgb);
+            using (var gr = Graphics.FromImage(bitmap24))
+            {
+                gr.DrawImage(bitmap, new Rectangle(0, 0, bitmap24.Width, bitmap24.Height));
+            }
+            return bitmap24;
+        }
+    }
+}
---
+++ ScreenToText/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing;
+
+namespace ScreenToText
+{
+    class Program
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+        static bool ctrlPressed = false;
+        static bool ctrlSPressed = false;
+
+        [STAThread]
+        [Obsolete]
+        public static void Main()
+        {
+            AppDomain.CurrentDomain.AppendPrivatePath(path: @"./libs");
+            _hookID = SetHook(_proc);
+            SnippingTool.AreaSelected += DoScreen;
+            Application.Run();
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        static void DoScreen(object sender, EventArgs e)
+        {
+            Image bmp = SnippingTool.Image;
+            if (bmp != null)
+            {
+                Console.WriteLine("Success: screenshot sent to clipboard!");
+                Clipboard.SetImage(bmp);
+                //ConvertImage.ConvertImageToTextIron();
+                ConvertImage.ConvertImageToTextVS();
+            }
+            else
+            {
+                Console.WriteLine("Error: no screenshot taken!");
+            }
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(
+            int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam) // Defining a keyboard shortcut
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                if (vkCode == 162 || vkCode == 163) // 162 is Left Ctrl, 163 is Right Ctrl
+                {
+                    ctrlPressed = true;
+                }
+                else if (vkCode == 83 && ctrlPressed == true) // "S"
+                {
+                    ctrlPressed = false;
+                    ctrlSPressed = true;
+                }
+                else if (vkCode == 68 && ctrlSPressed == true) // "D"
+                {
+                    ctrlPressed = false;
+                    ctrlSPressed = false;
+                    Console.WriteLine("\"CTRL + S + D\" is PRESSED!");
+                    SnippingTool.Snip();
+                }
+                else
+                {
+                    ctrlPressed = false;
+                    ctrlSPressed = false;
+                }
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}
